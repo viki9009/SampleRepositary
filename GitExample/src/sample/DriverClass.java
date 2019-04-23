79b9f4f6f68b9dc806bf52796f6c9ea3edd379de
VIKI <VIKI@DESKTOP-MO6TAM1>
2019-04-23 19:37:21 +0530

    Commit2

--- GitExample/src/sample/DriverClass.java
+++ GitExample/src/sample/DriverClass.java
@@ -5,6 +5,8 @@ public class DriverClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello program1");
+		
+		System.out.println("Hello program2 Merging Concept");
 	}
 
 }
